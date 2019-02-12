049b255b52d75a4841441910a9f5cc2c3968837c
mathwer <mat-wer@hotmail.com>
2019-02-12 21:52:31 -0200

    Initial commit

---
+++ Assets/BotãoURL.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotãoURL : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Botão()
+    {
+            Application.OpenURL("http://rereal.com.br/");
+            Debug.Log("Abrindo URL");  
+    }
+}
---
+++ Assets/SlideShow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideShow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Slide;
+    public GameObject CartaoCostas;
+
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActiveAndEnabled)
+        {
+            transform.parent = null;
+        }
+    }
+    public void Fechar()
+    {
+    Slide.transform.parent = CartaoCostas.transform;
+            
+    }
+}
